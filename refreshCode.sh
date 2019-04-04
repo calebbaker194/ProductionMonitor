6b037d0a3363392ae648b34e9e903d9509adeec7
caleb <caleb.baker194@gmail.com>
2019-04-04 13:13:30 -0500

    Update the code refresh

--- refreshCode.sh
+++ refreshCode.sh
@@ -5,5 +5,6 @@ cd /home/pi/src/ProductionMonitor
 git pull
 cp /home/pi/src/ProductionMonitor/* /home/pi/Desktop/Production
 cd /home/pi/Desktop/Production
+cp /home/pi/Desktop/Production/refreshCode.sh /home/pi/Desktop/
 sudo ./getupdates.sh
 /usr/bin/python3 prodmain.py 2>&1 >> logfile.data
